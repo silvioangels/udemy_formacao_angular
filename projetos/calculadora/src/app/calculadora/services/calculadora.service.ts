8153797343825c8493e0faad10a08943627e9975
Silvio Angelo <silvioangels@gmail.com>
2018-06-25 09:29:36 -0300

    Comentarios

--- projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -12,14 +12,26 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
   constructor() { }
-  /** */
+  /** 
+   * Defini as constantes utilizadas
+   * para identificar as operações de calculo
+  */
   static readonly SOMA: string = '+';
   static readonly SUBTRACAO: string = '-';
   static readonly DIVISAO: string = '/';
   static readonly MULTIPLICACAO: string = '*';
 
+
+  /**
+   * Metodo que calcula uma operação matematica
+   * dado dois numeros.
+   * @param num1 number
+   * @param num2 number
+   * @param operacao String Operacao a ser executada
+   * @return number Resultado da operação
+   */
   calcular(num1: number, num2: number, operacao: string): number {
-    let resultado: number;
+    let resultado: number; // armazena o resultado da operacao
 
     switch (operacao) {
       case CalculadoraService.SOMA:
